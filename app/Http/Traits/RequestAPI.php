d2b6b0acbc2e0c29b8d4d360a0a411f7d91360df
Yudo <53389941+juniz@users.noreply.github.com>
2022-10-20 11:56:42 +0700

    url api

--- app/Http/Traits/RequestAPI.php
+++ app/Http/Traits/RequestAPI.php
@@ -4,28 +4,29 @@ namespace App\Http\Traits;
 use Illuminate\Support\Facades\Http;
 
 trait RequestAPI {
-
+    protected $urlPost ='https://training-bios2.kemenkeu.go.id/api/ws/';
+    protected $urlGet = 'https://training-bios2.kemenkeu.go.id/api/get/data/';
     public function postData($url, $header, $body)
     {
-        $response = Http::asForm()->withHeaders($header)->post(env('URL_POST_DATA'). $url, $body);
+        $response = Http::asForm()->withHeaders($header)->post(env('URL_POST_DATA' ,$this->urlPost). $url, $body);
         return $response;
     }
 
     public function getData($url, $header)
     {
-        $response = Http::withHeaders($header)->post(env('URL_GET_DATA'). $url);
+        $response = Http::withHeaders($header)->post(env('URL_GET_DATA', $this->urlGet). $url);
         return $response;
     }
 
     public function getBank()
     {
-        $response = Http::get(env('URL_POST_DATA').'ref/bank');
+        $response = Http::get(env('URL_POST_DATA', $this->urlGet).'ref/bank');
         return $response;
     }
 
     public function getAkun()
     {
-        $response = Http::get(env('URL_POST_DATA').'ref/akun');
+        $response = Http::get(env('URL_POST_DATA', $this->urlGet).'ref/akun');
         return $response;
     }
 }
--- app/Http/Traits/Token.php
+++ app/Http/Traits/Token.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Http;
 
 trait Token {
     public function getToken() {
-        $response = Http::post(env('URL_TOKEN'),[
-            'satker' => env('SATKER'),
-            'key' => env('KEY')
+        $response = Http::post(env('URL_TOKEN', 'https://training-bios2.kemenkeu.go.id/api/token'),[
+            'satker' => env('SATKER', '679614'),
+            'key' => env('KEY', 'HE7FjyqfTfmh5N6ozvniuHxT9Ho530Rv')
         ]);
         return $response;
     }
