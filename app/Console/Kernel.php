5f5d6d7cba857bef4d134d6de67b8c9a7894fb37
Yudo <53389941+juniz@users.noreply.github.com>
2022-10-24 12:12:13 +0700

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,29 +22,24 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('cron:sdm')->daily()->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_sdm.log'));
-        // $schedule->command('cron:labparam')->daily('01:00')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:igd')->daily('01:30')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:operasi')->daily('02:00')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:penjab')->daily('02:30')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:poli')->daily('03:00')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:radiologi')->daily('03:30')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:ralan')->daily('04:00')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
+        $schedule->command('cron:sdm')->daily()->at('12:30')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_sdm.log'));
 
         $tanggal = Carbon::now()->subDay()->isoFormat('YYYY-MM-DD');
-        $jam = Carbon::now();
+        $jam = strtotime('12:50');
         $lab = $this->countLab($tanggal);
         foreach($lab as $l){
-            $time = "$jam->addMinutes(5)->isoFormat('HH:mm')";
+            $time = date("H:i", strtotime('+3 minutes', $jam));;
             $schedule->command('cron:labparam '.$l->kd_jenis_prw.' '.$l->jml)->daily()->at($time)->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
+            $jam = strtotime($time);
         }
+
         // $schedule->command('cron:labparam')->daily()->at('11:20')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:igd')->daily()->at('11:20')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:operasi')->daily()->at('11:20')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:penjab')->daily()->at('11:20')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:poli')->daily()->at('11:20')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:radiologi')->daily()->at('11:20')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
-        // $schedule->command('cron:ralan')->daily()->at('11:20')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
+        $schedule->command('cron:igd')->daily()->at('13:00')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
+        $schedule->command('cron:operasi')->daily()->at('13:20')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
+        $schedule->command('cron:penjab')->daily()->at('13:30')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
+        $schedule->command('cron:poli')->daily()->at('13:40')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
+        $schedule->command('cron:radiologi')->daily()->at('13:50')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
+        $schedule->command('cron:ralan')->daily()->at('14:00')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
     }
 
     /**
