b9afccf8f324bdd542e77cdefb74e0fcd18050fb
Yudo <53389941+juniz@users.noreply.github.com>
2022-11-22 15:22:17 +0700

    terbaru

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         
-        $layanan =$this->taskSetting('layanan');
-        $schedule->command('cron:layanan')->daily()->at($layanan->jam)->timezone('Asia/Jakarta')->appendOutputTo(storage_path($layanan->log));
+        // $layanan =$this->taskSetting('layanan');
+        $schedule->command('cron:layanan')->daily()->at('15:25')->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_layanan.log'));
         // $schedule->command('cron:sdm')->weekly()->timezone('Asia/Jakarta')->appendOutputTo(storage_path('logs/cron_sdm.log'));
 
         // $tanggal = Carbon::now()->subDay()->isoFormat('YYYY-MM-DD');
